a64e37cd4e75b013732392157c411ba7fff5c3b8
Ayame489 <ayame2420pablm@gmail.com>
2025-04-04 14:06:08 +0000

    Prediction Pipeline

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask , request , render_template
+import numpy as np 
+import pandas as pd 
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData , PredictPipeline
+
+# Creating the app 
+application_name = Flask(__name__)
+app = application_name
+
+## Route for home page : 
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata' , methods = ['GET' , 'POST'])
+def predict_datapoint() :
+    if request.method == 'GET' :
+        # Return default home page :
+        return render_template('home.html')
+    else : 
+        # Creating the data : 
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+
+        predict_pipeline = PredictPipeline() 
+        results = predict_pipeline.predict(pred_df)
+
+
+        return render_template('home.html' , results= results[0] )
+    
+
+
+# Run the app.py : 
+if __name__=="__main__":
+    app.run(host="0.0.0.0" , debug= True) 
+
+
+    
+
+    
+    
+
+
+
+
+
+
+    
